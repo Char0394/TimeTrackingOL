c7de08ca7773359888da4f85c8baa07e510ca1c1
Char0394 <Charline@MacBook-Pro-de-Charlin.local>
2014-01-26 18:12:48 -0400

    Mostrar nombre de usuario

--- trunk/TimeTracking/login.js
+++ trunk/TimeTracking/login.js
@@ -5,14 +5,15 @@
               document.querySelector('#signinButton').setAttribute('style', 'display:none');
 			  document.querySelector('#desconectar').setAttribute('style', 'display:block');
 			  
-			  var appscript = document.createElement('script');
+			  CallUserProfile();
+			  //Call app.js
+			  var appscript = document.createElement('script'); 
 	          appscript.type = 'text/javascript';
 	          appscript.async = true;
 	          appscript.src = 'app.js';
 	          var app = document.getElementsByTagName('script')[0];
 	          app.parentNode.insertBefore(appscript, app);
-			  
-              loadProfileInfo();
+			
           } else if (result['error']) {
              console.log(result);
 			 document.querySelector('#desconectar').setAttribute('style', 'display:none');
@@ -38,4 +39,28 @@ function logout()
 {
      document.location.href = "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://127.0.0.1:8000/TimeTracking/index.html";
         
+}
+
+//Call User Profile Info 
+function CallUserProfile() {
+      gapi.client.load('oauth2', 'v2', function() 
+	  {
+           gapi.client.oauth2.userinfo.get().execute(function(resp) {
+          // Shows user email
+          console.log(resp.email);
+        })
+      });
+
+      gapi.client.load('plus', 'v1', function() 
+	  {
+         gapi.client.plus.people.get( {'userId' : 'me'} ).execute(function(resp) 
+		 {
+          // Shows profile information
+		 // alert("Welcome "+ resp.displayName);
+		 var dn = resp.displayName;
+		  document.getElementById('dn').innerHTML="Welcome "+dn;
+          //console.log(resp);
+		  
+        })
+      });
 }
