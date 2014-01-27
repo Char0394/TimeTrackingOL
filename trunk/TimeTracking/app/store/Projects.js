b4d26deb36ba4f3efab69add4cc21800c8060327
bbycaber <thecaber@hotmail.com>
2014-01-27 12:02:48 +0100

    Json

--- trunk/TimeTracking/app/store/Projects.js
+++ trunk/TimeTracking/app/store/Projects.js
@@ -1,4 +1,13 @@
 Ext.define("TimeTracking.store.Projects",{
     extend:"Ext.data.Store",
-    model:"TimeTracking.model.project.Projects"
+    model:"TimeTracking.model.project.Projects",
+    autoLoad: true,
+    proxy : {
+                type : 'ajax',
+                url : 'data/Clients.json',
+                reader : {
+                            type : 'json',
+                            root : 'data'
+                         }
+            }
 });
