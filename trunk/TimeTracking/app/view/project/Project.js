c73e768002d5b1c46eb64c4e15c7f1fa4e3711f1
Cesar Contreras <cesarcontreras06@gmail.com>
2014-01-27 21:15:34 -0400

    Added Create New Project Form
    The form used to create a project is added to the Project.js class
    under view->project->Project.js

---
+++ trunk/TimeTracking/app/view/project/Project.js
@@ -0,0 +1,60 @@
+Ext.define('TimeTracking.view.project.Project', {
+    extend: 'Ext.form.Panel',
+    xtype: 'register-form',
+    
+    
+    frame: true,
+    title: 'Register',
+    bodyPadding: 10,
+    autoScroll:true,
+    width: 355,
+
+    fieldDefaults: {
+        labelAlign: 'right',
+        labelWidth: 115,
+        msgTarget: 'side'
+    },
+
+    initComponent: function() {
+        this.items = [{
+            xtype: 'fieldset',
+            title: 'Create New Project',
+            defaultType: 'textfield',
+            defaults: {
+                anchor: '100%'
+            },
+            items: [{
+            	xtype: 'combobox',
+                fieldLabel: 'Client',
+                name: 'Client',
+                //store: Ext.create(''),
+                valueField: 'abbr',
+                displayField: 'Client',
+                typeAhead: true,
+                queryMode: 'local',
+                emptyText: 'Select a client...',
+                
+            },
+            {
+                fieldLabel: 'Project Name',
+                emptyText: 'Define a name for the project',
+                name: 'projectName'
+                //allowBlank: false,
+            },
+            {
+            	xtype: 'textareafield',
+            	name: 'projectNotesTextArea',
+            	fieldLabel: 'Project Notes',
+            	value: 'Write notes in this field'
+        	},]
+        }];
+
+        this.callParent();
+    },
+
+    buttons: [{
+        text: 'Register',
+        disabled: true,
+        formBind: true
+    }]
+});
